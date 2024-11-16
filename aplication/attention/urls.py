850a3577797bf500aa088416dd789c4c1abe3813
SnayderCJ <Jcedenoj5@unemi.edu.ec>
2024-11-16 18:33:34 -0500

    Cita Medica

--- aplication/attention/urls.py
+++ aplication/attention/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from aplication.attention.views.medical_attention import AttentionCreateView, AttentionDetailView, AttentionListView, AttentionUpdateView
-from aplication.core.views.home import HomeTemplateView
-from aplication.core.views.patient import PatientCreateView, PatientDeleteView, PatientDetailView, PatientListView, PatientUpdateView
- 
+from aplication.attention.views.citaMedica import CitaMedicaCreateView, CitaMedicaListView, CitaMedicaUpdateView, CitaMedicaDeleteView, CitaMedicaDetailView
+
 app_name='attention' # define un espacio de nombre para la aplicacion
+
 urlpatterns = [
   # rutas de atenciones
   path('attention_list/',AttentionListView.as_view() ,name="attention_list"),
@@ -11,4 +11,11 @@ urlpatterns = [
   path('attention_update/<int:pk>/', AttentionUpdateView.as_view(),name='attention_update'),
   path('attention_detail/<int:pk>/', AttentionDetailView.as_view(),name='attention_detail'),
   # path('patient_delete/<int:pk>/', PatientDeleteView.as_view(),name='patient_delete'),
+  
+  # Cita Medica
+  path('cita_list/',CitaMedicaListView.as_view() ,name="citaMedica_list"),
+  path('cita_create/', CitaMedicaCreateView.as_view(),name="citaMedica_create"),
+  path('cita_update/<int:pk>/', CitaMedicaUpdateView.as_view(),name='citaMedica_update'),
+  path('cita_detail/<int:pk>/', CitaMedicaDetailView.as_view(),name='citaMedica_detail'),
+  path('cita_delete/<int:pk>/', CitaMedicaDeleteView.as_view(),name='citaMedica_delete'),
 ]
---
+++ aplication/attention/views/citaMedica.py
@@ -0,0 +1,113 @@
+from django.urls import reverse_lazy
+from aplication.attention.forms.citaMedica import CitaMedicaForm
+from aplication.attention.models import CitaMedica
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView, DetailView
+from django.http import JsonResponse
+from django.contrib import messages
+from django.db.models import Q
+from doctor.mixins import CreateViewMixin, DeleteViewMixin, ListViewMixin, UpdateViewMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
+from doctor.utils import save_audit
+
+class CitaMedicaListView(LoginRequiredMixin,ListViewMixin,ListView):
+    template_name = "attention/citaMedica/list.html"
+    model = CitaMedica
+    context_object_name = 'citas'
+    query = None
+    paginate_by = 10
+    
+    def get_queryset(self):
+        self.query = Q()
+        q1 = self.request.GET.get('q') # ver
+        
+        if q1 is not None: 
+            self.query.add(Q(fecha__icontains=q1), Q.AND)   
+        return self.model.objects.filter(self.query).order_by('fecha')
+    
+class CitaMedicaCreateView(LoginRequiredMixin, CreateViewMixin, CreateView):
+    model = CitaMedica
+    template_name = 'attention/citaMedica/form.html'
+    form_class = CitaMedicaForm
+    success_url = reverse_lazy('attention:citaMedica_list')
+    # permission_required = 'add_supplier' # en PermissionMixn se verfica si un grupo tiene el permiso
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['grabar'] = 'Grabar cita medica'
+        context['back_url'] = self.success_url
+        return context
+    
+    def form_valid(self, form):
+        # print("entro al form_valid")
+        response = super().form_valid(form)
+        citamedica = self.object
+        save_audit(self.request, citamedica, action='A')
+        messages.success(self.request, f"Éxito al crear la cita medica del paciente: {citamedica.paciente}.")
+        return response
+    
+    def form_invalid(self, form):
+        messages.error(self.request, "Error al enviar el formulario. Corrige los errores.")
+        print(form.errors)
+        return self.render_to_response(self.get_context_data(form=form))
+    
+class CitaMedicaUpdateView(LoginRequiredMixin, UpdateViewMixin, UpdateView):
+    model =CitaMedica
+    template_name = 'attention/citaMedica/form.html'
+    form_class = CitaMedicaForm
+    success_url = reverse_lazy('attention:citaMedica_list')
+    # permission_required = 'change_patient'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['grabar'] = 'Actualizar cita medica'
+        context['back_url'] = self.success_url
+        return context
+    
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        citamedica = self.object
+        save_audit(self.request, citamedica, action='M')
+        messages.success(self.request, f"Éxito al Modificar la cita medica del paciente : {citamedica.paciente}.")
+        print("mande mensaje")
+        return response
+    
+    def form_invalid(self, form):
+        messages.error(self.request, "Error al Modificar el formulario. Corrige los errores.")
+        print(form.errors)
+        return self.render_to_response(self.get_context_data(form=form))
+    
+class CitaMedicaDeleteView(LoginRequiredMixin, DeleteViewMixin, DeleteView):
+    model = CitaMedica
+    # template_name = 'core/patient/form.html'
+    success_url = reverse_lazy('attention:citaMedica_list')
+    # permission_required = 'delete_supplier'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['grabar'] = 'Eliminar cita medica'
+        context['description'] = f"¿Desea Eliminar la cita medica: {self.object.name}?"
+        context['back_url'] = self.success_url
+        return context
+    
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        success_message = f"Éxito al eliminar lógicamente la cita medica {self.object.name}."
+        messages.success(self.request, success_message)
+        # Cambiar el estado de eliminado lógico
+        # self.object.deleted = True
+        # self.object.save()
+        return super().delete(request, *args, **kwargs)
+    
+class CitaMedicaDetailView(LoginRequiredMixin,DetailView):
+    model = CitaMedica
+    
+    def get(self, request, *args, **kwargs):
+        citamedica = self.get_object()
+        data = {
+            'id': citamedica.id,
+            'fecha': citamedica.fecha,
+            'hora_cita': citamedica.hora_cita,
+            'estado': citamedica.estado,
+            # Añade más campos según tu modelo
+        }
+        return JsonResponse(data)
